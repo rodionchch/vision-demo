7702694c36eb5f93e32851f09b88251d4b8ceab0
Rodion <rodionchch@gmail.com>
2023-04-02 00:42:43 +0400

    Theming with React Navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {createContext, useCallback, useMemo, useState} from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
   MD3DarkTheme,
@@ -8,6 +12,7 @@ import {
   Provider as PaperProvider,
   adaptNavigationTheme,
 } from 'react-native-paper';
+import merge from 'deepmerge';
 import {Provider} from 'react-redux';
 import {store} from '../store';
 
@@ -15,31 +20,62 @@ import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen from '../screens/DetailsScreen';
 
 const Stack = createStackNavigator();
-const {LightTheme} = adaptNavigationTheme({reactNavigationLight: DefaultTheme});
+
+const {LightTheme, DarkTheme} = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+
+export const PreferencesContext = createContext({
+  toggleTheme: () => {},
+  isThemeDark: false,
+});
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [isThemeDark, setIsThemeDark] = useState(isDarkMode);
 
-  const theme = isDarkMode ? {...MD3DarkTheme} : {...MD3LightTheme};
+  const theme = isThemeDark
+    ? {...CombinedDarkTheme}
+    : {...CombinedDefaultTheme};
+
+  const toggleTheme = useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark],
+  );
 
   return (
     <Provider store={store}>
-      <PaperProvider theme={{...theme, version: 3}}>
-        <SafeAreaView>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        </SafeAreaView>
+      <PreferencesContext.Provider value={preferences}>
+        <PaperProvider theme={{...theme, version: 3}}>
+          <SafeAreaView>
+            <StatusBar
+              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            />
+          </SafeAreaView>
 
-        <NavigationContainer theme={LightTheme}>
-          <Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-              headerShown: false,
-            }}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </PaperProvider>
+          <NavigationContainer theme={theme}>
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PaperProvider>
+      </PreferencesContext.Provider>
     </Provider>
   );
 }
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
+import {PreferencesContext} from '../../components/App';
 
 const HomeScreen = () => {
   const {navigate} = useNavigation<any>();
+  const {toggleTheme, isThemeDark} = React.useContext(PreferencesContext);
 
   return (
     <View>
@@ -16,6 +18,7 @@ const HomeScreen = () => {
         }}>
         Go to Details
       </Button>
+      <Button onPress={toggleTheme}>ToggleTheme</Button>
     </View>
   );
 };
