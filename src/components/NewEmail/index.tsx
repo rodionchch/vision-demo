8ec277b661f044d6720edb82d246a888612c9193
Rodion <rodionchch@gmail.com>
2023-05-30 17:35:06 +0400

    Added New Email

--- src/components/NewEmail/index.tsx
+++ src/components/NewEmail/index.tsx
@@ -2,7 +2,36 @@ import React from 'react';
 import * as s from './styles';
 
 const NewEmail = () => {
-  return <s.NewEmail></s.NewEmail>;
+  return (
+    <s.NewEmail>
+      <s.NewEmailHeader>
+        <s.NewEmailHeaderItem>
+          <s.NewEmailHeaderItemLabel>To:</s.NewEmailHeaderItemLabel>
+          <s.NewEmailHeaderInput />
+        </s.NewEmailHeaderItem>
+        <s.NewEmailDivider />
+        <s.NewEmailHeaderItem>
+          <s.NewEmailHeaderItemLabel>From:</s.NewEmailHeaderItemLabel>
+          <s.NewEmailHeaderInput />
+        </s.NewEmailHeaderItem>
+        <s.NewEmailDivider />
+
+        <s.NewEmailHeaderItem>
+          <s.NewEmailHeaderItemLabel>CC:</s.NewEmailHeaderItemLabel>
+          <s.NewEmailHeaderInput />
+        </s.NewEmailHeaderItem>
+        <s.NewEmailDivider />
+
+        <s.NewEmailHeaderItem>
+          <s.NewEmailHeaderItemLabel>Subject:</s.NewEmailHeaderItemLabel>
+          <s.NewEmailHeaderInput />
+        </s.NewEmailHeaderItem>
+        <s.NewEmailDivider />
+      </s.NewEmailHeader>
+
+      <s.NewEmailTextArea />
+    </s.NewEmail>
+  );
 };
 
 export default NewEmail;
--- src/components/NewEmail/styles.ts
+++ src/components/NewEmail/styles.ts
@@ -1,4 +1,52 @@
 import styled from 'styled-components/native';
 import {ScrollView} from 'react-native-gesture-handler';
+import {Divider, List, MD3Colors, Text, TextInput} from 'react-native-paper';
 
 export const NewEmail = styled(ScrollView)``;
+
+export const NewEmailHeader = styled(List.Section)``;
+
+export const NewEmailHeaderItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 0 16px;
+`;
+
+export const NewEmailHeaderItemLabel = styled(Text).attrs({
+  variant: 'titleSmall',
+})`
+  margin-top: 2px;
+`;
+
+export const NewEmailHeaderInput = styled(TextInput).attrs({
+  mode: 'outlined',
+  outlineStyle: {
+    borderWidth: 0,
+  },
+})`
+  flex: 1;
+  background: transparent;
+  height: 32px;
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 8px;
+`;
+
+export const NewEmailTextArea = styled(TextInput).attrs({
+  mode: 'outlined',
+  multiline: true,
+  outlineStyle: {
+    borderWidth: 0,
+  },
+})`
+  flex: 1;
+  background: transparent;
+  font-size: 14px;
+  font-weight: 500;
+  min-height: 100%;
+  margin-bottom: 8px;
+`;
+
+export const NewEmailDivider = styled(Divider)`
+  margin-left: 16px;
+`;
